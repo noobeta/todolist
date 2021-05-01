0246bbb3ce84addcf5d4ef4fdfb31fdddde615aa
Bhatta Tarun Tanmay (IFIN CSS SCS D CEQ) <TarunTanmay.Bhatta@infineon.com>
2021-05-01 11:11:12 +0530

    first commit

---
+++ springboot-mongodb-youtube-playlist-master/src/main/java/in/bushansirgur/springbootmongodb/model/TodoDTO.java
@@ -0,0 +1,90 @@
+package in.bushansirgur.springbootmongodb.model;
+
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection="todos")
+public class TodoDTO {
+
+	
+	@Id
+	private String id;
+	
+	@NotNull(message="Todo cannot be null")
+	private String todo;
+	
+	@NotNull(message="Description cannot be null")
+	private String description;
+	
+	@NotNull(message="Completed cannot be null")
+	private Boolean completed;
+	
+	private Date createdAt;
+	
+	private Date updatedAt;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTodo() {
+		return todo;
+	}
+
+	public void setTodo(String todo) {
+		this.todo = todo;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Boolean getCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(Boolean completed) {
+		this.completed = completed;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
+	
+	
+}
